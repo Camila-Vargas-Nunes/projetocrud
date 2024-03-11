ef0713b981b02ca23e178f77a314b4a1df62dbfb
Camila <camila.vargasn@gmail.com>
2024-03-11 19:41:37 -0300

    controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+
+     */
+    public function index()
+    {
+        return Product::all();
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required'
+        ]);
+
+        return Product::create($request->all());
+    }
+
+    public function show(Product $product)
+    {
+        return $product;
+    }
+
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'required'
+        ]);
+
+        $product->update($request->all());
+
+        return $product;
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return response()->json(['message' => 'Product deleted successfully']);
+    }
+}
+
